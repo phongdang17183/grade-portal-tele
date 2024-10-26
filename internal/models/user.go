572efa12a3311dafc2ec87eb3ee36c171e213026
Huangfu1204 <Phuc.nguyenlehoang707@hcmut.edu.vn>
2024-10-26 19:07:19 +0700

    Change structure

--- internal/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
-	MSSV     string `json:"ms"`
-	Faculty  string `json:"faculty"`
-	Role     string `json:"role"`
-}
