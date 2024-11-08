b9b1380f779e57bbcbc6fe3d9c620aa8d809f6b2
Huangfu1204 <Phuc.nguyenlehoang707@hcmut.edu.vn>
2024-11-08 23:35:53 +0700

    Great change

---
+++ source/cmd/api/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "Grade_Portal_TelegramBot/internal/bot"
+)
+
+func main() {
+    bot.Start()
+}
---
+++ source/internal/bot/middleware.go
@@ -0,0 +1,4 @@
+// cmd/api/other.go
+package bot
+
+// File này tạm thời không sử dụng
