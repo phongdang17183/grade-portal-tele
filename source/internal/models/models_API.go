82356a34f2905544065783b644af95f807e809e4
phongdang17183 <dangquocphong1703@gmail.com>
2024-12-03 00:12:27 +0700

    Update models_API.go

--- source/internal/models/models_API.go
+++ source/internal/models/models_API.go
@@ -23,9 +23,9 @@ type InfoSV struct {
 }
 
 type Score struct {
-	BT  *float64 `json:"BT"`
-	TN  *float64 `json:"TN"`
-	BTL *float64 `json:"BTL"`
+	BT  []*float64 `json:"BT"`
+	TN  []*float64 `json:"TN"`
+	BTL []*float64 `json:"BTL"`
 	GK  *float64 `json:"GK"`
 	CK  *float64 `json:"CK"`
 }
